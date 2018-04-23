e611eea7c6abad492fdca98bfd254b54e9dab76d
Half_Code <nishantgsingh07@gmail.com>
2018-04-23 19:40:38 +0530

    Initial Commit

--- BuiltInFunctionalInterfaces/AddingElementToArrayBiConsumer.java
+++ BuiltInFunctionalInterfaces/AddingElementToArrayBiConsumer.java
@@ -8,7 +8,10 @@ public static void main(String[] args) {
 																     //to the method, not sending some parameters like we call a method method(1,2)
 																	//. Here we are doing something like this. A method(int a,int b) is defined. We are
 																	//passing a method body with parameters along with its declaration i.e we make new
-	
+//We can similarly do other operations	
+	printSum(arr,3,(key,element) -> System.out.println(key*element)); 
+	printSum(arr,3,(key,element) -> System.out.println(key/element)); 
+	printSum(arr,3,(key,element) -> System.out.println(key-element)); 
 }
 public static void printSum(int[] arr,int key,BiConsumer<Integer,Integer> bi)
 {
